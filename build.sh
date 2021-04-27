665731a63bfc6d180578b4d27912ef2e3d0d197d
pjentsch <pjentsch@uwaterloo.ca>
2021-04-27 11:19:34 -0400

    fix frontpage ubug in build script

--- build.sh
+++ build.sh
@@ -4,12 +4,11 @@ rm -rf build_files
 mkdir build_files
 
 cp organizing_stuff/* build_files/
-cp thesis.tex build_files/thesis.tex
-cp -r -u front_matter build_files/front_matter
+cp -r -u front_matter/* build_files/
 cp -r -u chapter_1 build_files/chapter_1
 cp -r -u chapter_2 build_files/chapter_2
 cp -r -u chapter_3 build_files/chapter_3
-cp -r -u appendices build_files/appendices
+cp -r -u appendices/* build_files
 cd build_files
 pdflatex thesis 
 bibtex thesis
